a077bf49bc365811181679462cf5b332eed4a555
rsaha10 <60122555+rsaha10@users.noreply.github.com>
2021-01-14 16:00:14 -0500

    import Pillow

--- stegoCode.py
+++ stegoCode.py
@@ -1,4 +1,7 @@
 import numpy
+import Pillow
+
+image = PIL.Image.open("file name") //fill in file name
 
 def userInput(input):
     binary = ''.join(format(ord(i), 'b') for i in input)
@@ -9,3 +12,6 @@ def imageInfo(input, imgName):
     msgBits = len(input) * 8
 
 userInput("ABCdefg")
+
+def rgbValue (image):
+    
