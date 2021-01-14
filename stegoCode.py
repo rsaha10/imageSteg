0b925716c261064683ecacd20ab0ebc61e6df661
rsaha10 <rsaha10@stuy.edu>
2021-01-13 20:23:20 -0500

    first commit

---
+++ stegoCode.py
@@ -0,0 +1,11 @@
+import numpy
+
+def userInput(input):
+    for char in input:
+        binary = ''.join(format(ord(i), 'b') for i in input)
+        for power in range (7, -1, -1):
+            yield 1 if binary & 2 ** power else 0
+
+
+def imageInfo(input, imgName):
+    msgBits = len(input) * 8
