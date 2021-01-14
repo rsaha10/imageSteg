e3b5b5ff731d7229e66e2484d42bf740e38df8ec
rsaha10 <rsaha10@stuy.edu>
2021-01-14 16:51:04 -0500

    array of rgb values

--- stegoCode.py
+++ stegoCode.py
@@ -1,12 +1,22 @@
-import numpy
+import numpy as np
 import PIL
 from PIL import Image
 
-image = PIL.Image.open("file name") #fillinfilename
+filename = "green.jpg"
+width, height = Image.open(filename).size
+totalPix =  width * height
+
+
+image = PIL.Image.open(filename) #fillinfilename
+imageRGB = image.convert("RGB")
+pixelRGBval = imageRGB.getpixel((10,15))
+
+numpy_array = np.array(image)
+print (numpy_array)
 
 def userInput(input):
     binary = ''.join(format(ord(i), 'b') for i in input)
-    print(binary)
+    #print(binary)
 
 
 def imageInfo(input, imgName):
