d55f354723644f90ad3d0ac79f3bd66ddfb85cee
Tanzebruh <tanzirhasanhasan@gmail.com>
2021-01-14 18:18:23 -0500

    This makes sense now

--- stegoCode.py
+++ stegoCode.py
@@ -2,29 +2,29 @@ import numpy as np
 import PIL
 from PIL import Image
 
-
-
-#print (numpy_array[1][2][2])
-
 def userInput(msg):
     binary = ''.join(format(ord(i), 'b') for i in msg)
+    
     #print(binary)
 
-
 def imageInfo(msg, filename):
-    msgBits = len(msg) * 8
     width, height = Image.open(filename).size
     totalPix =  width * height
+    binary = userInput(msg)
+    bitlength = len(binary)
+    if (bitlength + 48 < totalPix):
+        encode(binary, image)
+
+userInput("ABCdefg")
+
+def encode (binary, image):
     imageRGB = image.convert("RGB")
     pixelRGBval = imageRGB.getpixel((10,15))
     numpy_array = np.array(image)
+    #print (numpy_array[1][2][2])
     for i in range (width):
         for j in range (height):
             if (numpy_array[i][j][2]%2 != 0):
                 numpy_array[i][j][2] -=1 
     PIL_image = Image.fromarray(numpy_array.astype('uint8'), 'RGB')
-    PIL_image.save("test.jpg")
-
-userInput("ABCdefg")
-
-#def rgbValue (image):
+    #PIL_image.save("test.jpg")
