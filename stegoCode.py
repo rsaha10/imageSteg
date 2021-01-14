4e027ff94142da146bf0ea4161519b29e750ad3d
Tanzebruh <tanzirhasanhasan@gmail.com>
2021-01-14 17:59:19 -0500

    Some changes, modifies all the Blue values to all be even, setting up the floor for changing the least significant bits

--- stegoCode.py
+++ stegoCode.py
@@ -2,25 +2,29 @@ import numpy as np
 import PIL
 from PIL import Image
 
-filename = "green.jpg"
-width, height = Image.open(filename).size
-totalPix =  width * height
 
+#PIL_image.save("test.jpg")
+#print (numpy_array[1][2][2])
 
-image = PIL.Image.open(filename) #fillinfilename
-imageRGB = image.convert("RGB")
-pixelRGBval = imageRGB.getpixel((10,15))
-
-numpy_array = np.array(image)
-print (numpy_array)
-
-def userInput(input):
-    binary = ''.join(format(ord(i), 'b') for i in input)
+def userInput(msg):
+    binary = ''.join(format(ord(i), 'b') for i in msg)
     #print(binary)
 
 
-def imageInfo(input, imgName):
-    msgBits = len(input) * 8
+def imageInfo(msg, filename):
+    msgBits = len(msg) * 8
+    width, height = Image.open(filename).size
+    totalPix =  width * height
+    image = Image.open(filename)
+    image = Image.open(filename) #fillinfilename
+    imageRGB = image.convert("RGB")
+    pixelRGBval = imageRGB.getpixel((10,15))
+    numpy_array = np.array(image)
+    for i in range (width):
+        for j in range (height):
+            if (numpy_array[i][j][2]%2 != 0):
+                numpy_array[i][j][2] -=1 
+    PIL_image = Image.fromarray(numpy_array.astype('uint8'), 'RGB')
 
 userInput("ABCdefg")
 
