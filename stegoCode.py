ce344f1477368ec6ba3d2b350fd52c247db9324c
Tanzebruh <thasan10@stuy.edu>
2021-01-15 20:46:04 -0500

    this took too long

--- stegoCode.py
+++ stegoCode.py
@@ -1,32 +1,55 @@
 import numpy as np
 import PIL
 from PIL import Image
+import binascii
 
 def userInput(msg):
-    binary = ''.join(format(ord(i), 'b') for i in msg)
+    binary = '0' + '0'.join(format(ord(i), 'b') for i in msg)
+    for i in range (len(binary)%8):
+        print (chr(int(binary[:8],2)))
+    print(len(binary))
     return binary
 
-def imageInfo(msg, filename):
+def encode(msg, filename):
     image = Image.open(filename)
     width, height = image.size
     totalPix =  width * height
     binary = userInput(msg)
     bitlength = len(binary)
-    if (bitlength + 48 < totalPix):
-        encode(binary, image, width, height)
+    if (bitlength < totalPix):
+        encodeBit(binary, image, width, height)
+        print("Encoded")
+    else:
+        print("message too long or image too small")
 
-userInput("ABCdefg")
-
-def encode (binary, image, width, height):
+def encodeBit(binary, image, width, height):
     imageRGB = image.convert("RGB")
     pixelRGBval = imageRGB.getpixel((10,15))
     numpy_array = np.array(image)
-    #print (numpy_array[1][2][2])
-    for i in range (width):
-        for j in range (height):
-            if (numpy_array[i][j][2]%2 != 0):
-                numpy_array[i][j][2] -=1 
+    for i in range(len(binary)):
+        h = i%height
+        w = i//height
+        if (numpy_array[w][h][2]%2 != 0):
+            numpy_array[w][h][2] -=1
+        numpy_array[w][h][2] += int(binary[w + h])
     PIL_image = Image.fromarray(numpy_array.astype('uint8'), 'RGB')
-    #PIL_image.save("test.jpg")
+    PIL_image.save("test.jpg")
+    
+def decode(filename):
+    image = Image.open(filename)
+    width, height = image.size
+    numpy_array = np.array(image)
+    binary = ""
+    count = 0
+##    for i in range (width):
+##        for j in range (height):
+##            count +=1
+##            binary += str(numpy_array[i][j][2]%2)
+##            if (count%8 ==0)
+##    print (len(binary)%8)
+##    answer = chr(int(binary, 2))
+##    print (answer)
+        
+encode("test", "green.jpg")
+decode("test.jpg")
 
-imageInfo("test", "green.jpg")
