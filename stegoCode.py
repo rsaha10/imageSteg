86278ad59f6cafea9c78fa277482943c453e39b7
Tanzebruh <thasan10@stuy.edu>
2021-01-16 00:09:06 -0500

    this is frustrating

--- stegoCode.py
+++ stegoCode.py
@@ -1,13 +1,15 @@
 import numpy as np
 import PIL
 from PIL import Image
-import binascii
-
 def userInput(msg):
+    msg += "aspscv"
     binary = '0' + '0'.join(format(ord(i), 'b') for i in msg)
-    for i in range (len(binary)%8):
-        print (chr(int(binary[:8],2)))
-    print(len(binary))
+    count = 0
+    for i in range (len(binary)//8):
+        count +=1
+        print("check")
+        print (chr(int(binary[8*(count-1):8*count],2)))
+    print(binary)
     return binary
 
 def encode(msg, filename):
@@ -17,39 +19,61 @@ def encode(msg, filename):
     binary = userInput(msg)
     bitlength = len(binary)
     if (bitlength < totalPix):
-        encodeBit(binary, image, width, height)
+        numpy_array = np.array(image)
+        image.close()
+        encodeBit(binary, numpy_array, width, height)
         print("Encoded")
     else:
         print("message too long or image too small")
 
-def encodeBit(binary, image, width, height):
-    imageRGB = image.convert("RGB")
-    pixelRGBval = imageRGB.getpixel((10,15))
-    numpy_array = np.array(image)
+def encodeBit(binary, numpy_array, width, height):
     for i in range(len(binary)):
         h = i%height
         w = i//height
-        if (numpy_array[w][h][2]%2 != 0):
-            numpy_array[w][h][2] -=1
-        numpy_array[w][h][2] += int(binary[w + h])
-    PIL_image = Image.fromarray(numpy_array.astype('uint8'), 'RGB')
-    PIL_image.save("test.jpg")
+        if (numpy_array[w, h, 1]%2 != 0):
+            numpy_array[w, h, 1] = numpy_array[w, h, 1] - 1
+        numpy_array[w, h, 1] += int(binary[w + h])
+        #print(int(binary[w + h]))
+##        print ("w: ", w, "h: ", h, "value ", numpy_array[w, h, 1])
+        #print(numpy_array[w][h][2])
+    PIL_image = Image.fromarray(numpy_array.copy().astype(np.uint8))
+    PIL_image.save("test.png")
+    PIL_image.close()
+    image = Image.open("test.png")
+    newarr = np.array(image)
+    image.close()
+    for i in range(len(binary)):
+        h = i%height
+        w = i//height
+        print ("w: ", w, "h: ", h, "value ", newarr[w, h, 1])
     
-def decode(filename):
-    image = Image.open(filename)
-    width, height = image.size
-    numpy_array = np.array(image)
-    binary = ""
-    count = 0
-##    for i in range (width):
-##        for j in range (height):
-##            count +=1
-##            binary += str(numpy_array[i][j][2]%2)
-##            if (count%8 ==0)
-##    print (len(binary)%8)
-##    answer = chr(int(binary, 2))
-##    print (answer)
-        
-encode("test", "green.jpg")
-decode("test.jpg")
 
+def decode(filename):
+    Dimage = Image.open(filename)
+    width, height = Dimage.size
+    numpy_array = np.array(Dimage)
+    binary = ""
+    answer = ""
+    found = False
+    for i in range(80):
+        h = i%height
+        w = i//height
+        print ("w: ", w, "h: ", h, "value ", numpy_array[w, h, 1])
+    for w in range (width):
+        if (not found):
+            for h in range (height):
+                binary += str(numpy_array[w][h][1]%2)
+                #print(binary)
+                if (len(binary)==8):
+                    answer += chr(int(binary,2))
+                    print (binary)
+                    #print (chr(int(binary,2)))
+                    binary = ""
+                if ("aspscv" in answer):
+                    found = True
+                    print("lol")
+                    break
+    print (answer)
+
+encode("test", "green.jpg")
+decode("test.png")
